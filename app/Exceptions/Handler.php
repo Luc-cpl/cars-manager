d9c8802c5c1534a66819020f133c42b344f079e1
Luc-cpl <lucas.carvalhopl@outlook.com>
2024-01-20 23:44:26 -0300

    change: error handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,6 @@ namespace App\Exceptions;
 
 use Illuminate\Database\UniqueConstraintViolationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Http\JsonResponse;
 use InvalidArgumentException;
 use Throwable;
 
@@ -29,46 +28,20 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
-    }
 
-    public function render($request, Throwable $exception)
-    {
-        if ($exception instanceof InvalidArgumentException) {
-            return $this->handleInvalidArgumentException($exception);
-        }
-
-        if ($exception instanceof UniqueConstraintViolationException) {
-            return $this->handleUniqueConstraintViolationException($exception);
-        }
-
-        if ($exception instanceof InvalidPasswordException) {
-            return $this->handleInvalidPasswordException($exception);
-        }
-
-        return parent::render($request, $exception);
-    }
-
-    protected function handleInvalidArgumentException(InvalidArgumentException $exception)
-    {
-        return new JsonResponse([
+        $this->renderable(fn (InvalidArgumentException $exception) => response()->json([
             'error' => $exception->getMessage(),
             'code' => 'invalid_argument',
-        ], 400);
-    }
+        ], 400));
 
-    protected function handleUniqueConstraintViolationException(UniqueConstraintViolationException $exception)
-    {
-        return new JsonResponse([
+        $this->renderable(fn (UniqueConstraintViolationException $exception) => response()->json([
             'error' => 'Unique constraint violation',
             'code' => 'unique_constraint_violation',
-        ], 400);
-    }
+        ], 400));
 
-    protected function handleInvalidPasswordException(InvalidPasswordException $exception)
-    {
-        return new JsonResponse([
+        $this->renderable(fn (InvalidPasswordException $exception) => response()->json([
             'error' => 'Invalid password',
             'code' => 'invalid_password',
-        ], 400);
+        ], 400));
     }
 }
